4752a75821024718d7acbfebc9dfecb658c4dc10
Manas Rachh <mrachh@ccmlin043.flatironinstitute.org>
2021-04-28 09:11:37 -0400

    updating test_vol_vs_pt_fmm and fixed tree coeff code

--- src/Common/tree_vol_coeffs.f
+++ src/Common/tree_vol_coeffs.f
@@ -1739,7 +1739,7 @@ c     Rearrange old arrays now
             centers(3,curbox) = tcenters(3,ibox)
             do i=1,npbox
               do idim=1,nd
-                fvals(idim,i,ibox) = tfvals(idim,i,ibox)
+                fvals(idim,i,curbox) = tfvals(idim,i,ibox)
               enddo
             enddo
             iflag(curbox) = tiflag(ibox)
@@ -1755,7 +1755,7 @@ c     Rearrange old arrays now
             nchild(curbox) = tnchild(ibox)
             do i=1,npbox
               do idim=1,nd
-                fvals(idim,i,ibox) = tfvals(idim,i,ibox)
+                fvals(idim,i,curbox) = tfvals(idim,i,ibox)
               enddo
             enddo
             iflag(curbox) = tiflag(ibox)
